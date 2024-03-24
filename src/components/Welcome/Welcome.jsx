f40dd3df08d980d794fd4ee847fe072ef48bffc4
direct66612 <113045624+direct66612@users.noreply.github.com>
2024-03-24 14:07:29 -0400

    init

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,7 +4,7 @@ import { Title, WelcomeContainer } from "./Welcome.styled";
 export const Welcome = () => {
   return (
     <WelcomeContainer>
-      <Title>welcome to our car rental website, you can go to our:</Title>
+      <Title>welcome to our car rental website, you can go too our:</Title>
       <Link className="catalog" to={"/catalog"}>
         CATALOG
       </Link>
